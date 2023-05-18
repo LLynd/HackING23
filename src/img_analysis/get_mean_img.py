defa8ce24aa71cae009cf0ba3a965ebbec05b4a3
ADKulesza <ad.kulesza122@gmail.com>
2023-05-18 15:31:19 +0200

    get hist and mean img

--- src/img_analysis/get_mean_img.py
+++ src/img_analysis/get_mean_img.py
@@ -15,13 +15,16 @@ for i, class_dir in enumerate(directory_list):
     _path = os.path.join(PATH, class_dir)
 
     img_collection = skimage.io.imread_collection(_path + PATTERN)
-    img_array = np.zeros((len(img_collection), *SIZE), dtype=np.uint8)
-    img_mean_output = np.zeros(SIZE, dtype=np.uint8)
+    num_img = len(img_collection)
+    img_array = np.zeros((num_img, *SIZE), dtype=np.float64)
+    img_mean_output = np.zeros(SIZE, dtype=np.float64)
     for i, img_path in enumerate(img_collection.files):
+        print(class_dir, i, "/", num_img)
         img_array[i, :, :] = skimage.io.imread(img_path)
-    img_mean_output[i] = np.mean(img_array, axis=0)
+    img_mean_output = np.mean(img_array, axis=0)
     skimage.io.imsave(class_dir + "_mean.png", img_mean_output)
+
     plt.figure()
-    plt.hist(img_array.ravel(), bins=256)
+    plt.hist(img_array.ravel(), bins=64)
     plt.savefig(class_dir + "_hist.png")
     plt.close()
