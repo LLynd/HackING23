0a273a02a53b2c288ac498d8c3b092f88b28ed43
ADKulesza <ad.kulesza122@gmail.com>
2023-05-18 14:35:00 +0200

    get hist and mean img

--- src/img_analysis/get_mean_img.py
+++ src/img_analysis/get_mean_img.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 
 PATH = "F:\Studia\INGHackathon\HackING23\src\data\datasets\\train_set_resize"
 
-
 PATTERN = "\*"
 SIZE = (800, 600)
 directory_list = os.listdir(PATH)
@@ -19,10 +18,10 @@ for i, class_dir in enumerate(directory_list):
     img_array = np.zeros((len(img_collection), *SIZE), dtype=np.uint8)
     img_mean_output = np.zeros(SIZE, dtype=np.uint8)
     for i, img_path in enumerate(img_collection.files):
-        img_array[i,:,:] = skimage.io.imread(img_path)
+        img_array[i, :, :] = skimage.io.imread(img_path)
     img_mean_output[i] = np.mean(img_array, axis=0)
     skimage.io.imsave(class_dir + "_mean.png", img_mean_output)
     plt.figure()
-    plt.hist(img_array.ravel(), bins = 256)
+    plt.hist(img_array.ravel(), bins=256)
     plt.savefig(class_dir + "_hist.png")
     plt.close()
